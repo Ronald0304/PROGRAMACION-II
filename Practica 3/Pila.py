b4859204781386fcbeb6f61ff373e7a307de72a7
Ronald0304 <160867694+Ronald0304@users.noreply.github.com>
2025-03-03 21:35:55 -0400

    Update Pila.py

--- Practica 3/Pila.py
+++ Practica 3/Pila.py
@@ -1,30 +1,34 @@
 class Pila:
-    def __init__(self, n):
+    def __init__(self, n: int):
         self.n = n
         self.arreglo = [0] * n
         self.top = -1
 
-    def push(self, e):
+    def push(self, e: int):
         if not self.isFull():
             self.top += 1
             self.arreglo[self.top] = e
         else:
-            print("Pila llena")
+            print("La pila está llena")
 
-    def pop(self):
+    def pop(self) -> int:
         if not self.isEmpty():
             valor = self.arreglo[self.top]
             self.top -= 1
             return valor
         else:
-            print("Pila vacía")
+            print("La pila está vacía")
             return -1
 
-    def peek(self):
-        return -1 if self.isEmpty() else self.arreglo[self.top]
+    def peek(self) -> int:
+        if not self.isEmpty():
+            return self.arreglo[self.top]
+        else:
+            print("La pila está vacía")
+            return -1
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
         return self.top == -1
 
-    def isFull(self):
+    def isFull(self) -> bool:
         return self.top == self.n - 1
